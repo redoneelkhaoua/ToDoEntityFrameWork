00d860383d38677d8e740f6289c0accec01a15e9
ilyass.lamrani <ilyass@outlook.com>
2022-03-02 22:32:51 +0100

    refactor

---
+++ back/Todo.DependencyInjection/ServiceCollectionExtentions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Todo.Domain.Models;
+using Todo.Repository;
+using Todo.RepositoryAbstraction;
+using ToDo.Services;
+
+namespace Todo.DeoendencyInjection
+{
+    public static class ServiceCollectionExtentions
+    {
+        public static IServiceCollection AddTodoList(this IServiceCollection services)
+        {
+            services.AddScoped<IToDoRepository, TaskToDoRepository>();
+            services.AddScoped<IToDoService, ToDoService>();
+            // todo une connection par request => open à reception de request / fermeture une fois une reponse est donnée
+           
+            services.AddDbContext<TodoAppContext>(o =>
+            {
+                o.UseNpgsql();
+            });
+            return services;
+        }
+    }
+}
---
+++ back/Todo.Domain/TodoTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Todo.Domain.Models
+{
+    
+    public partial class TodoTask
+    {
+        public decimal Id { get; set; }
+        public string Status { get; set; }
+        public string Details { get; set; }
+    }
+}
